5a749f6be84b59dc04a2955c7efb4cb49aa91169
lu782663119 <823653011@qq.com>
2022-05-05 22:38:55 +0800

    完善功能

---
+++ first/captcha.py
@@ -0,0 +1,53 @@
+"""
+图⽚验证码
+"""
+import os
+import random
+from io import BytesIO
+from PIL import Image
+from PIL import ImageFilter
+from PIL.ImageDraw import Draw
+from PIL.ImageFont import truetype
+
+
+
+
+class Bezier:
+    """⻉塞尔曲线"""
+    def __init__(self):
+        self.tsequence = tuple([t / 20.0 for t in range(21)])
+        self.beziers = {}
+    def make_bezier(self, n):
+        """绘制⻉塞尔曲线"""
+        try:
+            return self.beziers[n]
+        except KeyError:
+            combinations = pascal_row(n - 1)
+            result = []
+            for t in self.tsequence:
+                tpowers = (t ** i for i in range(n))
+                upowers = ((1 - t) ** i for i in range(n - 1, -1, -1))
+                coefs = [c * a * b for c, a, b in zip(combinations,tpowers, upowers)]
+
+                result.append(coefs)
+        self.beziers[n] = result
+        return result
+class Captcha:
+    """验证码"""
+    def __init__(self, width, height, fonts=None, color=None):
+        self._image = None
+        self._fonts = fonts if fonts else \
+            [os.path.join(os.path.dirname(__file__), 'fonts', font)
+             for font in ['Arial.ttf', 'Georgia.ttf', 'Action.ttf']]
+        self._color = color if color else random_color(0, 200, random.randint(220, 255))
+
+        self._width, self._height = width, height
+    @classmethod
+    def instance(cls, width=200, height=75):
+        """用于获取Captcha对象类的方法"""
+        prop_name = f'_instance_{width}_{height}'
+        if not hasattr(cls, prop_name):
+            setattr(cls, prop_name, cls(width, height))
+        return getattr(cls, prop_name)
+
+
